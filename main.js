e18a837073646adf7c63874f1fc546b692812a7c
BaraaMohammad <karajahbara4@gmail.com>
2024-02-25 12:52:13 +0200

    create project

---
+++ main.js
@@ -0,0 +1,197 @@
+//get total
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+console.log(title,price,taxes,ads,discount,total,count,category,submit);
+let mood = 'creat';
+let tmp ;
+//get total
+function gettotal(){
+if(price.value != '' ){
+    let result = (+price.value + +taxes.value + +ads.value) - +discount.value;
+        total.innerHTML = result;
+    total.style.backgroundColor = '#040';
+}
+else{
+    total.innerHTML = '';
+    total.style.background = "#a00d02"
+}
+}
+//creat product
+let datapro;
+if(localStorage.product != null ){
+    datapro = JSON.parse(localStorage.product);
+}else{
+datapro=[];
+}
+submit.onclick = function(){
+    let newprod = {
+title:title.value,price:price.value,taxes:taxes.value,
+ads:ads.value,discount:discount.value,total:total.innerHTML,count:count.value,category:category.value,
+    }
+    //count
+    if(title.value != ''){
+    if ( mood === 'create'){
+    if(newprod.count > 1){
+        for ( let k = 0 ; k<newprod.count ; k++){
+            datapro.push(newprod);
+        }
+    }
+    else{
+    datapro.push(newprod);
+    }}
+    else{
+        datapro[tmp]=newprod;
+        mood = 'create';
+        submit.innerHTML= 'create';
+        count.style.display = 'block';
+    }}
+    //save local storage
+    localStorage.setItem('product', JSON.stringify(datapro));
+    cleardata();
+    showdara();
+}
+//clear inputs
+function cleardata(){
+    title.value='';
+    price.value='';
+    taxes.value='';
+    ads.value='';
+    discount.value='';
+    count.value='';
+    total.innerHTML='';
+    category.value='';
+}
+//read 
+let table = '';
+function showdara(){
+    gettotal();
+
+for( let i =0;i<datapro.length;i++){
+    table += `
+    <tr>
+        <td>${i}</td>
+        <td>${datapro[i].title}</td>
+        <td>${datapro[i].price}</td>
+        <td>${datapro[i].taxes}</td>
+        <td>${datapro[i].ads}</td>
+        <td>${datapro[i].discount}</td>
+        <td>${datapro[i].total}</td>
+        <td>${datapro[i].count}</td>
+        <td>${datapro[i].category}</td>
+        <td><button onclick="updatedata(${i})" id="update">update</button></td>
+        <td><button onclick="deletedata(${i})" id="delete">delete</button></td> 
+    </tr>
+    `;
+
+}
+document.getElementById('tbody').innerHTML = table;
+let btndelete = document.getElementById('deletall');
+if(datapro.length > 0){
+    btndelete.innerHTML = `
+<button onclick = "deletall()">delete( ${datapro.length})</button>
+    `;
+}
+else{
+    btndelete.innerHTML = '';  
+}
+}
+showdara();
+//delete
+ function deletedata(i){
+datapro.splice(i,1);
+localStorage.product = JSON.stringify(datapro);
+showdara();
+ }
+ function deletall(){
+    localStorage.clear();
+    datapro.splice(0);
+    showdara();
+ }
+ 
+//update
+function updatedata(i){
+    title.value = datapro[i].title;
+    price.value = datapro[i].price;
+    taxes.value = datapro[i].taxes;
+    ads.value = datapro[i].ads;
+    discount.value = datapro[i].discount;
+    gettotal();
+    count.style.display = 'none';
+    category.value = datapro[i].category;
+    submit.innerHTML = 'update'
+mood = 'update';
+tmp = i;
+scroll({
+    top:0,
+    behavior : 'smooth'
+})
+}
+//search
+let searchmood = 'title';
+function getsermood(id){
+    let search = document.getElementById('search');
+if(id === 'searchTitle'){
+    searchmood = 'title';
+    search.placeholder = 'Search By Title';
+
+}
+else{
+    searchmood = 'Category';
+    search.placeholder = 'Search By Category';
+}
+search.focus();
+}
+function searchdatae(value){
+    let table = '';
+    if(    searchmood == 'title' ){
+        for(let i = 0;i< datapro.length;i++){
+if(datapro[i].title.includes(value)){
+    console.log('ho');
+    table += `
+    <tr> 
+        <td>${i}</td>
+        <td>${datapro[i].title}</td>
+        <td>${datapro[i].price}</td>
+        <td>${datapro[i].taxes}</td>
+        <td>${datapro[i].ads}</td>
+        <td>${datapro[i].discount}</td>
+        <td>${datapro[i].total}</td>
+        <td>${datapro[i].count}</td>
+        <td>${datapro[i].category}</td>
+        <td><button onclick="updatedata(${i})" id="update">update</button></td>
+        <td><button onclick="deletedata(${i})" id="delete">delete</button></td> 
+    </tr>
+    `;
+}
+}}
+    else{
+            for(let i = 0;i< datapro.length;i++){
+                if(datapro[i].category.includes(value)){
+                    table += `
+                    <tr>
+                        <td>${i}</td>
+                        <td>${datapro[i].title}</td>
+                        <td>${datapro[i].price}</td>
+                        <td>${datapro[i].taxes}</td>
+                        <td>${datapro[i].ads}</td>
+                        <td>${datapro[i].discount}</td>
+                        <td>${datapro[i].total}</td>
+                        <td>${datapro[i].count}</td>
+                        <td>${datapro[i].category}</td>
+                        <td><button onclick="updatedata(${i})" id="update">update</button></td>
+                        <td><button onclick="deletedata(${i})" id="delete">delete</button></td> 
+                    </tr>
+                    `;
+                }   
+            }
+    }
+    document.getElementById('tbody').innerHTML = table;
+}
+//clean data
